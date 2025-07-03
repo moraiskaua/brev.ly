1c140a7f321adc4c4bf092083b73118b8244dcf0
moraiskaua <moraiskaua03@gmail.com>
2025-07-03 11:32:44 -0300

    chore: config style guide

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <img src='/src/assets/logo.svg' alt='Brev.ly' className='w-1/2' />
-    </div>
+    <main className='container mx-auto'>
+      <div className='flex flex-col items-center justify-center h-screen'></div>
+    </main>
   );
 }
